9f2c35093e054f0ea10de9ee3b5021291e89f465
sivaprasadvitta <sivaprasad.vitta@gmial.com>
2024-09-19 22:34:26 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose'
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("mongoose is connected");
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 const app = express();
 const port =3000;
