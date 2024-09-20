d9f7fbfe8efc916143c4ab9e021e6ab1172d0e84
sivaprasadvitta <sivaprasad.vitta@gmial.com>
2024-09-20 22:10:49 +0530

    create signup page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,41 +12,45 @@ function Header() {
                 to="/"
                 className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
             >
-                <span className="px-2 py-1 bg-gradient-to-r from bg-indigo-500 via bg-purple-500 to-pink-500 rounded-lg text-white">
+                <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
                     SV
                 </span>
                 Blog
             </Link>
-            <form>
+
+            <form className="hidden lg:block mx-auto">
                 <TextInput
                     type="text"
                     placeholder="Search..."
                     rightIcon={AiOutlineSearch}
-                    className="hidden lg:inline"
+                    className="dark:text-gray-300 dark:bg-gray-700"
                 />
             </form>
-            <Button className="w-12 h-10 " color="gray" pill>
-                <AiOutlineSearch />
-            </Button>
 
-            <div className="flex gap-2 md:order-2">
-                <Button className="w-12 h-10 hidden sm:inline " color="grey" pill>
+            <div className="flex items-center gap-2 md:order-2">
+                <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+                    <AiOutlineSearch />
+                </Button>
+
+                <Button className="w-12 h-10 hidden sm:inline-block" color="gray" pill>
                     <FaMoon />
                 </Button>
+                
                 <Link to="/sign-in">
                     <Button gradientDuoTone="purpleToBlue">Sign In</Button>
                 </Link>
-                <Navbar.Toggle/>
+
+                <Navbar.Toggle />
             </div>
-            
+
             <Navbar.Collapse>
-                <Navbar.Link active={path === '/' } as={'div'}>
+                <Navbar.Link active={path === '/'} as="div">
                     <Link to="/">Home</Link>
                 </Navbar.Link>
-                <Navbar.Link active ={path === '/about'} as={'div'}>
+                <Navbar.Link active={path === '/about'} as="div">
                     <Link to="/about">About</Link>
                 </Navbar.Link>
-                <Navbar.Link active={path === '/active'} as={'div'}>
+                <Navbar.Link active={path === '/projects'} as="div">
                     <Link to="/projects">Projects</Link>
                 </Navbar.Link>
             </Navbar.Collapse>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, TextInput, Button, Label } from "flowbite-react";
 
 function SignUp() {
+
+  
   return (
-    <div>SignUp</div>
-  )
+    <div className="min-h-screen mt-20 bg-gray-50 dark:bg-gray-900 flex items-center justify-center">
+      <div className="flex flex-col md:flex-row gap-8 p-6 max-w-4xl w-full bg-white dark:bg-gray-800 shadow-lg rounded-lg">
+
+        {/* Left side */}
+        <div className="flex-1">
+          <Link to="/" className="font-bold text-4xl dark:text-white">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              SV
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5 text-gray-600 dark:text-gray-400">
+            Welcome to my blog. Sign up to explore more!
+          </p>
+        </div>
+
+        {/* Right side */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-6">
+            <div>
+              <Label value="Your Username" />
+              <TextInput type="text" placeholder="Username" id="username" className="mt-1" />
+            </div>
+            <div>
+              <Label value="Your Email" />
+              <TextInput type="email" placeholder="Email" id="email" className="mt-1" />
+            </div>
+            <div>
+              <Label value="Your Password" />
+              <TextInput type="password" placeholder="Password" id="password" className="mt-1" />
+            </div>
+            <Button type="submit" className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white mt-4">
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have and account?</span>
+            <Link to='/sign-in' className="text-blue-500">
+            Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
